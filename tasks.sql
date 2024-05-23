062d34e8082a084cbe72bb6cdce8a9540efc6638
Czolg25 <69245647+Czolg25@users.noreply.github.com>
2024-05-23 22:17:42 +0200

    Update tasks.sql

--- tasks.sql
+++ tasks.sql
@@ -6,7 +6,7 @@ select Username from Users order by Username;
 
 --zad b
 --sprawdzenie czy istnieje taki użytkownik oraz czy podał poprawne hasło
-select Username from Users where Username like 'john_doe' and Password like 'password123';
+select Username from Users where Username like 'john_doe' and Password like pwdencrypt('password123');
 --Wyświetla kategorie zaczynające się od C
 select CategoryName from Categories where CategoryName like 'C%';
 
@@ -17,6 +17,16 @@ select Title from Videos where  Views > 5000;
 select Username from Users where Cast(DateJoined as Date) = Cast(GETDATE() as Date);
 
 --zad d
+SELECT UserID, Username, Email, DateJoined FROM Users WHERE DateJoined >= DATEADD(DAY, -30, GETDATE());
+WITH CommentDifferences AS (
+    SELECT VideoID,
+           DATEDIFF(DAY, LAG(CommentDate) OVER (PARTITION BY VideoID ORDER BY CommentDate), CommentDate) AS DaysBetweenComments
+    FROM Comments
+)
+SELECT VideoID,
+       AVG(CAST(DaysBetweenComments AS FLOAT)) AS AvgDaysBetweenComments
+FROM CommentDifferences
+GROUP BY VideoID;
 
 --zad e
 --kto ile ma subskrybcji
@@ -32,3 +42,27 @@ select * from Users inner join Videos on Users.UserID = Videos.UserID inner join
 
 --zad g
 select *  from Users left join Subscriptions on Subscriptions.ChannelID =Users.UserID  ; 
+
+--zad f
+SELECT u.UserID, u.Username, u.Email, u.DateJoined, v.VideoCount
+FROM Users u
+JOIN (
+    SELECT UserID, COUNT(VideoID) AS VideoCount
+    FROM Videos
+    GROUP BY UserID
+    HAVING COUNT(VideoID) > (SELECT AVG(VideoCount) FROM (SELECT COUNT(VideoID) AS VideoCount FROM Videos GROUP BY UserID) AS SubQuery)
+) v ON u.UserID = v.UserID;
+
+SELECT v.VideoID, v.Title, v.Description, v.UploadDate, v.Views, v.Likes, v.Dislikes, c.CommentCount
+FROM Videos v
+JOIN (
+    SELECT VideoID, COUNT(CommentID) AS CommentCount
+    FROM Comments
+    GROUP BY VideoID
+    HAVING COUNT(CommentID) > (SELECT AVG(CommentCount) FROM (SELECT COUNT(CommentID) AS CommentCount FROM Comments GROUP BY VideoID) AS SubQuery)
+) c ON v.VideoID = c.VideoID;
+--dodatek
+begin transaction;
+EXEC deleteUser @UserID = 4;
+select * from Users
+rollback transaction;
