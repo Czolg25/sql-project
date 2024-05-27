a3ce87459fd7831c3f4a17932a610045d9b0296d
Czolg25 <69245647+Czolg25@users.noreply.github.com>
2024-05-27 14:48:51 +0200

    Update prodedures and triggers.sql

--- prodedures and triggers.sql
+++ prodedures and triggers.sql
@@ -41,75 +41,72 @@ CREATE PROCEDURE checkComment
 AS
 BEGIN
     BEGIN TRANSACTION;
-
     BEGIN TRY
-DECLARE @BadWorlds TABLE ([World] VARCHAR(20));
-INSERT INTO @BadWorlds VALUES ('Example'),('Bad'),('world');
+	DECLARE @BadWorlds TABLE ([World] VARCHAR(20));
+	INSERT INTO @BadWorlds VALUES ('Example'),('Bad'),('world');
 
-        DECLARE @Comment TABLE (
-UserID INT  NOT NULL,
-VideoID INT NOT NULL,
-CommentText VARCHAR(500) NOT NULL
-);
+	DECLARE @Comment TABLE (
+		UserID INT  NOT NULL,
+		VideoID INT NOT NULL,
+		CommentText VARCHAR(500) NOT NULL
+	);
 
-INSERT INTO @Comment SELECT UserID,VideoID,CommentText FROM Comments WHERE commentID = @commentID;
+	INSERT INTO @Comment SELECT UserID,VideoID,CommentText FROM Comments WHERE commentID = @commentID;
 
-        DECLARE @CommentText VARCHAR(500) = (SELECT CommentText FROM @Comment);
-print @CommentText;
-SET @CommentText = LOWER(@CommentText);
+			DECLARE @CommentText VARCHAR(500) = (SELECT CommentText FROM @Comment);
+	SET @CommentText = LOWER(@CommentText);
 
-SET @CommentText = REPLACE(REPLACE(REPLACE(LOWER(@CommentText),'.',''),'!',''),',',''); -- example chars to delete
+	SET @CommentText = REPLACE(REPLACE(REPLACE(LOWER(@CommentText),'.',''),'!',''),',',''); -- example chars to delete
 
-DECLARE @Word VARCHAR(100);
-DECLARE @Pos INT;
-DECLARE @Len INT;
+	DECLARE @Word VARCHAR(100);
+	DECLARE @Pos INT;
+	DECLARE @Len INT;
 
-SET @Pos = CHARINDEX(' ', @CommentText);
-SET @Len = LEN(@CommentText);
+	SET @Pos = CHARINDEX(' ', @CommentText);
+	SET @Len = LEN(@CommentText);
 
-WHILE @Len > 0
-BEGIN
-IF @Pos > 0
-BEGIN
-SET @Word = LEFT(@CommentText, @Pos - 1);
+	WHILE @Len > 0
+	BEGIN
+		IF @Pos > 0
+		BEGIN
+			SET @Word = LEFT(@CommentText, @Pos - 1);
 
-IF EXISTS (SELECT 1 FROM @BadWorlds WHERE World LIKE @Word)
-BEGIN
-INSERT INTO CommentWarnings(UserID,VideoID,CommentText) VALUES ((SELECT UserID FROM @Comment),(SELECT VideoID FROM @Comment),@CommentText);
-COMMIT TRANSACTION;
-RETURN;
-END
+			IF EXISTS (SELECT 1 FROM @BadWorlds WHERE World LIKE @Word)
+			BEGIN
+				INSERT INTO CommentWarnings(UserID,VideoID,CommentText) VALUES ((SELECT UserID FROM @Comment),(SELECT VideoID FROM @Comment),@CommentText);
+				COMMIT TRANSACTION;
+				RETURN;
+			END
 
-SET @CommentText = RIGHT(@CommentText, @Len - @Pos);
+			SET @CommentText = RIGHT(@CommentText, @Len - @Pos);
 
-SET @Pos = CHARINDEX(' ', @CommentText);
-SET @Len = LEN(@CommentText);
-END
-ELSE
-BEGIN
-SET @Word = @CommentText;
+			SET @Pos = CHARINDEX(' ', @CommentText);
+			SET @Len = LEN(@CommentText);
+		END
+		ELSE
+			BEGIN
+			
+			SET @Word = @CommentText;
 
+			IF EXISTS (SELECT 1 FROM @BadWorlds WHERE World LIKE @Word)
+			BEGIN
+				INSERT INTO CommentWarnings(UserID,VideoID,CommentText) VALUES ((SELECT UserID FROM @Comment),(SELECT VideoID FROM @Comment),@CommentText);
+				COMMIT TRANSACTION;
+				RETURN;
+				END
 
-IF EXISTS (SELECT 1 FROM @BadWorlds WHERE World LIKE @Word)
-BEGIN
-INSERT INTO CommentWarnings(UserID,VideoID,CommentText) VALUES ((SELECT UserID FROM @Comment),(SELECT VideoID FROM @Comment),@CommentText);
-COMMIT TRANSACTION;
-RETURN;
-END
+			SET @Len = 0;
+		END
+	END
 
-SET @Len = 0;
-END
-END
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION;
-       
-        THROW;
-    END CATCH
+	COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+	   
+		THROW;
+	END CATCH
 END;
-TRIGGER
 
 CREATE TRIGGER AddCommentWarningOnInsertComment
 ON Comments
@@ -136,19 +133,19 @@ ON Comments
 FOR UPDATE
 AS
 BEGIN
-DECLARE @CommentID INT;
-DECLARE CommentCursor CURSOR FOR (SELECT CommentID FROM inserted);
-
-OPEN CommentCursor;
-FETCH NEXT FROM CommentCursor INTO @CommentID;
-WHILE @@FETCH_STATUS = 0
-BEGIN
-EXEC checkComment @commentID = @CommentID;
-FETCH NEXT FROM CommentCursor INTO @CommentID;
-END;
-
-CLOSE CommentCursor;
-DEALLOCATE CommentCursor;
+    DECLARE @CommentID INT;
+    DECLARE CommentCursor CURSOR FOR (SELECT CommentID FROM inserted);
+    
+    OPEN CommentCursor;
+    FETCH NEXT FROM CommentCursor INTO @CommentID;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        EXEC checkComment @commentID = @CommentID;
+        FETCH NEXT FROM CommentCursor INTO @CommentID;
+    END;
+    
+    CLOSE CommentCursor;
+    DEALLOCATE CommentCursor;
 END;
 
 
